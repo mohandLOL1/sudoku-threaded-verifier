b4f25f9ba3bdc5747c5d5636a1ce9f96e5a5aabe
moatasem-1 <mutasemmohamed24@gmail.com>
2025-11-27 23:36:48 +0200

    Update ColumnVerifier.java

--- src/threads/ColumnVerifier.java
+++ src/threads/ColumnVerifier.java
@@ -1,32 +1,52 @@
 package threads;
+import model.SudokuBoard;
 
 public class ColumnVerifier implements ThreadVerifier {
 
-    private final int[][] board;
-    private final int num;
-    private final int col; 
+    private SudokuBoard board;
+    private boolean valid = true;
     
-    public ColumnVerifier(int[][] board, int num, int col) {
+    public ColumnVerifier() {
 
         this.board = board;
-        this.col = col;
-        this.num = num;
+    }
+    
 
+    @Override
+    public void run() {
+        for (int i = 0; i < 9 && valid; i++) {
+            int[] col = board.getColumn(i);
+
+            if (!isvalid_num(col)) {
+                System.out.println("Invalid Column at index: " + i);
+
+                valid = false;
+                break;
+            }
+        }
     }
     
     @Override
     public boolean isValid() {
-        for (int i = 0; i < 9; i++) {
-            if (board[i][col] == num) {
-                return false;
-            }
-        }
-        return true;
+
+        return valid;
     }
     
-    
-    @Override
-    public void run(){
-     isValid();
+    public boolean isvalid_num(int[] arr) {
+
+        boolean[] n = new boolean[10];
+
+        for (int i = 0; i < arr.length; i++) {
+            int num = arr[i];
+
+            if (num < 1 || num > 9) {
+                return false;
+            }
+            if (n[num]) {
+                return false;
+            }
+            n[num] = true;
+        }
+        return true;
     }
 }
