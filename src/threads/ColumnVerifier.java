c185be246e43bd4b07f247aad028f2a81dd51de1
moatasem-1 <mutasemmohamed24@gmail.com>
2025-11-27 13:13:49 +0200

    ColumnVerifier

--- src/threads/ColumnVerifier.java
+++ src/threads/ColumnVerifier.java
@@ -2,13 +2,31 @@ package threads;
 
 public class ColumnVerifier implements ThreadVerifier {
 
-    @Override
-    public void run(){
+    private final int[][] board;
+    private final int num;
+    private final int col; 
+    
+    public ColumnVerifier(int[][] board, int num, int col) {
+
+        this.board = board;
+        this.col = col;
+        this.num = num;
 
     }
-
+    
     @Override
     public boolean isValid() {
-        return false;
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    
+    @Override
+    public void run(){
+     isValid();
     }
 }
