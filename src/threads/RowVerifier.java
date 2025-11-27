285b5bf34cfdee154c805868a976a25baf834892
moatasem-1 <mutasemmohamed24@gmail.com>
2025-11-27 23:37:24 +0200

    Update RowVerifier.java

--- src/threads/RowVerifier.java
+++ src/threads/RowVerifier.java
@@ -1,32 +1,56 @@
 package threads;
+import model.SudokuBoard;
 
 public class RowVerifier implements ThreadVerifier {
-
-    private final int[][] board;
-    private final int num;
-    private final int row; 
     
-    public RowVerifier(int[][] board, int num, int row) {
+    private SudokuBoard board;
+    private boolean valid = true;
+
+
+    
+    public RowVerifier(SudokuBoard board) {
 
         this.board = board;
-        this.row = row;
-        this.num = num;
     }
     
     
-    @Override
-    public boolean isValid() {
-        for (int i = 0; i < 9; i++) {
-            if (board[row][i] == num) {
-                return false;
-            }
-        }
-        return true;
-    }
+
     
     @Override
     public void run(){
-       isValid();
+        for (int i = 0; i < 9; i++) {
+            int[] row = board.getRow(i);
+
+            if (!isvalid_num(row)) {
+                System.out.println("Invalid row at index: " + i);
+
+                valid = false;
+                break;
+            }
+    }
+  }
+    
+    @Override
+    public boolean isValid() {
+       return valid;
+    }
+    
+    public boolean isvalid_num(int[] arr) {
+
+        boolean[] n = new boolean[10];
+
+        for (int i = 0; i < arr.length; i++) {
+            int num = arr[i];
+
+            if (num < 1 || num > 9) {
+                return false;
+            }
+            if (n[num]) {
+                return false;
+            }
+            n[num] = true;
+        }
+        return true;
     }
 
 
