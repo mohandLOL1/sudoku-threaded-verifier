2632510c2a72ec837dc84772aa78c5e257d7aa30
mohand <mohandamr2019@gmail.com>
2025-11-28 05:30:06 +0200

    fixed synchronization issue

--- src/modes/TwentySevenThreadVerificationMode.java
+++ src/modes/TwentySevenThreadVerificationMode.java
@@ -9,31 +9,52 @@ import java.util.ArrayList;
 
 public class TwentySevenThreadVerificationMode implements VerificationMode {
 
-
     @Override
+
     public void verify(SudokuBoard board) {
 
-        ArrayList<UnitVerifier> threads = new ArrayList<>();
+        ArrayList<RowUnitVerifier> rowThreads = new ArrayList<>();
+        ArrayList<ColumnUnitVerifier> colThreads = new ArrayList<>();
+        ArrayList<BoxUnitVerifier> boxThreads = new ArrayList<>();
 
+        // create threads
         for (int i = 0; i < 9; i++) {
-            threads.add(new RowUnitVerifier(board, i));
-            threads.add(new ColumnUnitVerifier(board, i));
-            threads.add(new BoxUnitVerifier(board, i));
+            rowThreads.add(new RowUnitVerifier(board, i));
+            colThreads.add(new ColumnUnitVerifier(board, i));
+            boxThreads.add(new BoxUnitVerifier(board, i));
         }
 
-        for (UnitVerifier t : threads) t.start();
+        // start threads
+        rowThreads.forEach(Thread::start);
+        colThreads.forEach(Thread::start);
+        boxThreads.forEach(Thread::start);
+
+        // join threads
         try {
-            for (UnitVerifier t : threads) t.join();
-
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            rowThreads.forEach(t -> {
+                try { t.join(); } catch (InterruptedException e) { throw new RuntimeException(e); }
+            });
+            colThreads.forEach(t -> {
+                try { t.join(); } catch (InterruptedException e) { throw new RuntimeException(e); }
+            });
+            boxThreads.forEach(t -> {
+                try { t.join(); } catch (InterruptedException e) { throw new RuntimeException(e); }
+            });
+        } catch (RuntimeException e) {
+            throw e;
         }
 
+        // collect results
         ArrayList<FailedVerificationResult> allFailures = new ArrayList<>();
-        for (UnitVerifier t : threads) allFailures.addAll(t.getFailures());
-        for(FailedVerificationResult failedVerificationResult : allFailures){
-            System.out.println(failedVerificationResult);
-        }
+        rowThreads.forEach(t -> allFailures.addAll(t.getFailures()));
+        colThreads.forEach(t -> allFailures.addAll(t.getFailures()));
+        boxThreads.forEach(t -> allFailures.addAll(t.getFailures()));
+
+        // print results
+        allFailures.forEach(System.out::println);
     }
 
-}
+
+
+    }
+
