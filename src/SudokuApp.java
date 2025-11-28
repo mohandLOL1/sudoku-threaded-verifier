9c7dd26f8ed7107aba83dee448e961416a8dfe40
mohand <mohandamr2019@gmail.com>
2025-11-28 04:58:17 +0200

    final code version

--- src/SudokuApp.java
+++ src/SudokuApp.java
@@ -4,8 +4,7 @@ import modes.*;
 import java.io.IOException;
 
 public class SudokuApp {
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) {
         if (args.length < 2) {
             System.out.println("Usage: java -jar <appname>.jar <csvPath> <mode>");
             return;
@@ -14,16 +13,19 @@ public class SudokuApp {
         String csvPath = args[0];
         String modeName = args[1];
 
-        SudokuBoard board = SudokuBoard.fromCSV(csvPath);
         try {
-            VerificationMode mode = VerificationModeFactory.getMode(modeName);
+            SudokuBoard board = SudokuBoard.fromCSV(csvPath); // could throw IOException
+            VerificationMode mode = VerificationModeFactory.getMode(modeName); // could throw IllegalArgumentException
+
             long start = System.nanoTime();
             mode.verify(board);
             long end = System.nanoTime();
 
             System.out.printf("Verification took %.3f ms%n", (end - start) / 1_000_000.0);
-        } catch(IllegalArgumentException e) {
-            System.out.println("Invalid mode of verifications, please choose either: sequential, 3-threaded or 27-threaded");
+        } catch (IOException e) {
+            System.out.println("Error: Could not read CSV file at " + csvPath);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid mode of verification, please choose either: sequential, 3-threaded, or 27-threaded");
         }
     }
 }
